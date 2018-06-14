d0c35c709ae840c7816cf91e6fd6ad4138cc4868
Alexey Semigradsky <alexey.semigradsky@gmail.com>
2018-06-14 18:04:33 +0300

    Add `ScreenshotPlugin` design

--- lib/widgets/plugins/screenshot_plugin.dart
+++ lib/widgets/plugins/screenshot_plugin.dart
@@ -7,7 +7,37 @@ class ScreenshotPlugin extends Plugin {
     return new ListTile(
       leading: new Icon(Icons.desktop_windows),
       title: const Text("Получение снимка экрана"),
-      onTap: () {},
+      onTap: () {
+        final route = new MaterialPageRoute(
+          builder: (BuildContext context) {
+            return new _ScreenshotPluginPage();
+          },
+        );
+        Navigator.of(context).push(route);
+      },
+    );
+  }
+}
+
+class _ScreenshotPluginPage extends StatefulWidget {
+  @override
+  _ScreenshotPluginPageState createState() => new _ScreenshotPluginPageState();
+}
+
+class _ScreenshotPluginPageState extends State<_ScreenshotPluginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text(
+          'Скриншот экрана',
+          style: const TextStyle(fontSize: 18.0),
+        ),
+//        elevation: 0.0,
+      ),
+      body: new Center(
+        child: new Image.network('https://i.imgur.com/YhQnG48.jpg'),
+      ),
     );
   }
 }
