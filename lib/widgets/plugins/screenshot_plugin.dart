616e4a8f5697e9b7d7def83308b1497f5ad9dd4d
Alexey Semigradsky <alexey.semigradsky@gmail.com>
2018-06-15 13:56:08 +0300

    Integrate `ScreenshotPlugin`

--- lib/widgets/plugins/screenshot_plugin.dart
+++ lib/widgets/plugins/screenshot_plugin.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:airy_connect/models/device.dart';
 import 'package:airy_connect/plugin.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +14,7 @@ class ScreenshotPlugin extends Plugin {
       onTap: () {
         final route = new MaterialPageRoute(
           builder: (BuildContext context) {
-            return new _ScreenshotPluginPage();
+            return new _ScreenshotPluginPage(device);
           },
         );
         Navigator.of(context).push(route);
@@ -20,11 +24,29 @@ class ScreenshotPlugin extends Plugin {
 }
 
 class _ScreenshotPluginPage extends StatefulWidget {
+  final Device device;
+
+  _ScreenshotPluginPage(this.device);
+
   @override
   _ScreenshotPluginPageState createState() => new _ScreenshotPluginPageState();
 }
 
 class _ScreenshotPluginPageState extends State<_ScreenshotPluginPage> {
+  Future<Uint8List> _getScreenshot() async {
+    final device = widget.device;
+    final host = device.host;
+    final url = Uri.parse('https://$host:8420/'
+        'screenshotPlugin/'
+        'get');
+    print(url);
+    final httpClient = widget.device.httpClient;
+    final request = await httpClient.getUrl(url);
+    final response = await request.close();
+    final encodedBase64 = await response.transform(utf8.decoder).join();
+    return base64.decode(encodedBase64);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -36,7 +58,119 @@ class _ScreenshotPluginPageState extends State<_ScreenshotPluginPage> {
 //        elevation: 0.0,
       ),
       body: new Center(
-        child: new Image.network('https://i.imgur.com/YhQnG48.jpg'),
+        child: FutureBuilder(
+          future: _getScreenshot(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final bytes = snapshot.data;
+              return SizedBox.expand(
+                child: ImageViewer(
+                  image: Image.memory(
+                    bytes,
+//                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ImageViewer extends StatefulWidget {
+  const ImageViewer({Key key, this.image}) : super(key: key);
+
+  final Image image;
+
+  @override
+  _ImageViewerState createState() => new _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _flingAnimation;
+  Offset _offset = Offset.zero;
+  double _scale = 1.0;
+  Offset _normalizedOffset;
+  double _previousScale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(vsync: this)
+      ..addListener(_handleFlingAnimation);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
+  // then the minimum offset value is w - _scale * w, h - _scale * h.
+  Offset _clampOffset(Offset offset) {
+    final Size size = context.size;
+    final Offset minOffset =
+        new Offset(size.width, size.height) * (1.0 - _scale);
+    return new Offset(
+        offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
+  }
+
+  void _handleFlingAnimation() {
+    setState(() {
+      _offset = _flingAnimation.value;
+    });
+  }
+
+  void _handleOnScaleStart(ScaleStartDetails details) {
+    setState(() {
+      _previousScale = _scale;
+      _normalizedOffset = (details.focalPoint - _offset) / _scale;
+      // The fling animation stops if an input gesture starts.
+      _controller.stop();
+    });
+  }
+
+  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = (_previousScale * details.scale).clamp(1.0, 4.0);
+      // Ensure that image location under the focal point stays in the same place despite scaling.
+      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
+    });
+  }
+
+  void _handleOnScaleEnd(ScaleEndDetails details) {
+    final double magnitude = details.velocity.pixelsPerSecond.distance;
+    if (magnitude < 800) return;
+    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
+    final double distance = (Offset.zero & context.size).shortestSide;
+    _flingAnimation = new Tween<Offset>(
+            begin: _offset, end: _clampOffset(_offset + direction * distance))
+        .animate(_controller);
+    _controller
+      ..value = 0.0
+      ..fling(velocity: magnitude / 1000.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onScaleStart: _handleOnScaleStart,
+      onScaleUpdate: _handleOnScaleUpdate,
+      onScaleEnd: _handleOnScaleEnd,
+      child: new ClipRect(
+        child: new Transform(
+          transform: new Matrix4.identity()
+            ..translate(_offset.dx, _offset.dy)
+            ..scale(_scale),
+          child: widget.image,
+        ),
       ),
     );
   }
