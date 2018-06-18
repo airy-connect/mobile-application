3b3b250e488e20e53a561c15d0a661c13230f88c
Alexey Semigradsky <alexey.semigradsky@gmail.com>
2018-06-18 15:14:57 +0300

    Update `PresentationControlPlugin`

--- lib/widgets/plugins/presentation_control_plugin.dart
+++ lib/widgets/plugins/presentation_control_plugin.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:airy_connect/models/device.dart';
 import 'package:airy_connect/plugin.dart';
 import 'package:flutter/material.dart';
@@ -41,19 +43,104 @@ class _PresentationControlPluginPageState
           child: Text('Управление презентациями'),
         ),
       ),
-      body: new ListView(
-        children: [
-          new ListTile(
-            leading: new Icon(Icons.arrow_forward),
-            title: new Text('Следующий слайд'),
-            onTap: _nextSlide,
+      body: Center(
+        child: Transform.rotate(
+          angle: math.pi / 4,
+          child: Transform.scale(
+            scale: 2.0,
+            child: Container(
+              width: 128.0,
+              height: 128.0,
+              child: Stack(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(64.0),
+                              ),
+                              color: Colors.white,
+                            ),
+                            width: 64.0,
+                            height: 64.0,
+                          ),
+                          GestureDetector(
+                            onTap: _prevSlide,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(64.0),
+                                ),
+                                color: Colors.blue,
+                              ),
+                              width: 64.0,
+                              height: 64.0,
+                              child: Transform.rotate(
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  color: Colors.white,
+                                ),
+                                angle: -math.pi / 4,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: _nextSlide,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(64.0),
+                                ),
+                                color: Colors.blue,
+                              ),
+                              width: 64.0,
+                              height: 64.0,
+                              child: Transform.rotate(
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                                angle: -math.pi / 4,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(64.0),
+                              ),
+                              color: Colors.white,
+                            ),
+                            width: 64.0,
+                            height: 64.0,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(32.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(64.0),
+                      ),
+                      color: Colors.white,
+                    ),
+                    width: 64.0,
+                    height: 64.0,
+                  ),
+                ],
+              ),
+            ),
           ),
-          new ListTile(
-            leading: new Icon(Icons.arrow_back),
-            title: new Text('Предыдущий слайд'),
-            onTap: _prevSlide,
-          ),
-        ],
+        ),
       ),
     );
   }
